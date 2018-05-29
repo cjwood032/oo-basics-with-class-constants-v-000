7b28343908a5aa66870338a31f34afaf98d30d0a
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-29 07:38:35 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,19 +6,6 @@ class Book
     @title = title
   end
   GENRES = []
-  describe 'GENRES' do
-  it 'keeps track of all genres' do
-    genres = ["Thriller", "Science Fiction", "Romance"]
-    genres.each_with_index do |genre, i|
-      book = Book.new("Book_#{i}")
-      book.genre = genre
-    end
- 
-    genres.each do |genre|
-      expect(Book::GENRES).to include(genre)
-    end
-  end
-end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
